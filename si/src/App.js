fd3545feca4d21957336e12163d2438fc4cba49e
HoSung23 <150013982+HoSung23@users.noreply.github.com>
2024-03-25 12:20:42 -0600

    Subi carpeta src del proyecto
    Subi el login para que se empiece a probar la conexion con la base de datos, en lo que se prueba la base de datos voy a trabajar en el resto de la parte visual del app

---
+++ si/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Loginbtn from './loginbtn.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Loginbtn />
+      </header>
+    </div>
+  );
+}
+
+export default App;
